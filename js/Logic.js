8894a5e15a26b64d795313d2016c36c661a4d2c7
popusa <danielmaged.3@gmail.com>
2021-10-22 00:40:42 +0200

    Added eraser button, button style, and function

--- js/Logic.js
+++ js/Logic.js
@@ -1,4 +1,7 @@
-const paper = document.querySelector('Grid');
+const paper = document.getElementById('Grid');
+const eraser = document.getElementById('Eraser');
+let erasetracker = 1;
+let chosencolor = "#000000";
 function GenerateGridBlocks(squares){
     for (let i = 0; i < squares; i++){
         let squareblock = document.createElement('div');
@@ -6,7 +9,15 @@ function GenerateGridBlocks(squares){
         squareblock.addEventListener('mouseover',ApplyColor(squareblock));
     }
 }
+eraser.addEventListener('click',function(){
+    erasetracker++;
+//    console.log(erasetracker);
+    if (erasetracker % 2 == 0){
+        chosencolor = "#FFFFFF";
+//    console.log("ERASER MODE: ON");
+    }
+});
 function ApplyColor(square){
-    square.style.backgroundColor = black;
+    square.style.backgroundColor = chosencolor;
 }
 GenerateGridBlocks(16);
