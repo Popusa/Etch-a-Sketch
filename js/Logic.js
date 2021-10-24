c142ed7484aab2549d89a720066ca7532c78a5d0
popusa <danielmaged.3@gmail.com>
2021-10-24 22:38:24 +0200

    finally fixed the issue with grid being valued as null now I can actually continue working on this project

--- js/Logic.js
+++ js/Logic.js
@@ -1,25 +1,31 @@
-const paper = document.getElementById('Grid');
-const eraser = document.getElementById('Eraser');
+const paper = document.querySelector('.Grid');
+const board = document.createElement('div');
+const eraser = document.querySelector('#Eraser');
 let erasetracker = 1;
 let chosencolor = "#000000";
 function GenerateGridBlocks(squares){
     let NumOfSquares = squares * squares;
     for (let i = 0; i < NumOfSquares; i++){
-        let squareblock = document.createElement("div");
+        let squareblock = document.createElement('div');
         squareblock.classList.add("grid");
-        paper.appendChild(squareblock);
+        board.appendChild(squareblock);
         squareblock.addEventListener('mouseover',ApplyColor(squareblock));
     }
 }
 eraser.addEventListener('click',function(){
     erasetracker++;
-//    console.log(erasetracker);
+    console.log(erasetracker);
     if (erasetracker % 2 == 0){
         chosencolor = "#FFFFFF";
-//    console.log("ERASER MODE: ON");
+        console.log("ERASER MODE: ON");
+    }
+    else{
+        chosencolor = "#FFFFFF";
+        console.log("ERASER MODE: OFF");
     }
 });
 function ApplyColor(square){
     square.style.backgroundColor = chosencolor;
 }
-//GenerateGridBlocks(16);
+GenerateGridBlocks(16);
+paper.appendChild(board);
