27e62d413d5212975414c0eca813d7c340f06d14
popusa <danielmaged.3@gmail.com>
2021-11-11 21:16:39 +0200

    fixed issue with eraser and added message box for drawing status

--- js/Logic.js
+++ js/Logic.js
@@ -2,20 +2,23 @@ const paper = document.querySelector('.Grid');
 const board = document.querySelector('.Container');
 const eraser = document.querySelector('#Eraser');
 const editgrid = document.querySelector('#EditGrid');
+const msg = document.querySelector('.msg');
 let erasetracker = 1;
-let gridsize = 4;
+let gridsize = 16;
 let chosencolor = "#000000";
 function GenerateGridBlocks(squares){
     let NumOfSquares = squares * squares;
     for (let i = 0; i < NumOfSquares; i++){
         let squareblock = document.createElement('div');
         squareblock.classList.add("item");
-        squareblock.addEventListener('mouseover',ApplyColor(squareblock));
+        squareblock.addEventListener('mouseover',function (){
+            squareblock.style.backgroundColor = chosencolor;
+        });
         board.appendChild(squareblock);
-        squareblock.style.width = `${576 / NumOfSquares}px`;
-        squareblock.style.height = `${192 / NumOfSquares}px`;
-        squareblock.style.backgroundColor = 'black';
+        squareblock.style.width = `${582 / NumOfSquares}px`;
+        squareblock.style.height = `${198 / NumOfSquares}px`;
     }
+    paper.appendChild(board);
 }
 function ResetGrid(parent){
     while (parent.firstChild) {
@@ -27,22 +30,18 @@ eraser.addEventListener('click',function(){
     console.log(erasetracker);
     if (erasetracker % 2 == 0){
         chosencolor = "#FFFFFF";
-        console.log("ERASER MODE: ON");
+        msg.innerText = "ERASER MODE: ON"; 
     }
     else{
-        chosencolor = "#FFFFFF";
-        console.log("ERASER MODE: OFF");
+        chosencolor = "#000000";
+        msg.innerText = "ARTISTIC MODE: ON";
     }
 });
 editgrid.addEventListener('click',function(){
-let newgridsize = prompt("Enter the grid size.");
-while (newgridsize <= 1 || newgridsize > 16)
-    newgridsize = prompt("Enter a valid value.");
+let newgridsize = prompt("Enter the grid size. Limit is 16");
+while (newgridsize <= 1 || newgridsize > 32)
+    newgridsize = prompt("Enter a valid value. Limit is 16");
 ResetGrid(board);
 GenerateGridBlocks(newgridsize);
 });
-function ApplyColor(squareblock){
-    squareblock.style.backgroundColor = chosencolor;
-}
 GenerateGridBlocks(gridsize);
-paper.appendChild(board);
